45bd2dea0302b3f7f8f041136a2168b9c621f62f
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-12 14:11:29 +0530

    phonebook-backend step2

--- index.js
+++ index.js
@@ -27,7 +27,13 @@ const persons = [
 ];
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  );
 });
 
 app.get("/api/persons", (req, res) => {
