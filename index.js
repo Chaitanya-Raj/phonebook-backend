5915a2c03a1d685a28b943d03dd917b5b7c005e2
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-13 21:16:21 +0530

    phonebook database step2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const Person = require("./models/person");
 
 morgan.token("data", function getData(req) {
   if (req.method === "POST") return JSON.stringify(req.body);
@@ -15,49 +17,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
-});
-
 app.get("/info", (req, res) => {
   res.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+    `<p>Phonebook has info for ${Person.length} people</p><p>${new Date()}</p>`
   );
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = +req.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) res.json(person);
-  else res.status(404).end();
+  Person.findById(req.params.id).then((person) => {
+    res.json(person.toJSON());
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -86,25 +61,25 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  if (
-    persons.find(
-      (person) => person.name.toLowerCase() === body.name.toLowerCase()
-    )
-  ) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  // if (
+  //   persons.find(
+  //     (person) => person.name.toLowerCase() === body.name.toLowerCase()
+  //   )
+  // ) {
+  //   return res.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+    // id: generateId(),
+  });
 
-  persons = persons.concat(person);
-
-  res.json(persons);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson.toJSON());
+  });
 });
 
 const PORT = process.env.PORT || 3001;
