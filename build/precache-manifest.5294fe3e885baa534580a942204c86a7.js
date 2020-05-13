2b7480e0584cf1bb1c4bd15ff81df01b1879ce0b
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-13 10:57:54 +0530

    add frontend

---
+++ build/precache-manifest.5294fe3e885baa534580a942204c86a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209c44226c719d589bad90756474b4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98bde4addb23d2f7b3c",
+    "url": "/static/css/main.614ace88.chunk.css"
+  },
+  {
+    "revision": "35658a72390815255e11",
+    "url": "/static/js/2.66c1b036.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66c1b036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98bde4addb23d2f7b3c",
+    "url": "/static/js/main.1e472bd6.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
---
+++ build/precache-manifest.6d3c0ddc3a9caceb1a256fe9ea4aeb28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9421986585369a2fc804a4d376b4ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3d9e8820daa6379de0",
+    "url": "/static/css/main.614ace88.chunk.css"
+  },
+  {
+    "revision": "35658a72390815255e11",
+    "url": "/static/js/2.66c1b036.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66c1b036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3d9e8820daa6379de0",
+    "url": "/static/js/main.42245dd6.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
